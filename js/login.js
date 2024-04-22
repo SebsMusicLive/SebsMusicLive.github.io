999fcbc1f5cd053ae8e0761fbea1ba7451349ded
SebsMusicLive <lopezortegajohansebastian@gmail.com>
2024-04-22 11:37:38 -0500

    Tercer commit

--- js/login.js
+++ js/login.js
@@ -1,15 +1,17 @@
-function ingresar(){
-    var codigo = document.querySelector("#codigo").value;
-    var contrasena = document.querySelector("#contrasena").value;
-}
+const urlStudents = "https://24a0dac0-2579-4138-985c-bec2df4bdfcc-00-3unzo70c406dl.riker.replit.dev/students";
+const codigo = document.querySelector("#codigo").value;
+const contrasena = document.querySelector("#contrasena").value;
 
-function estudiantes(){
-    fetch('https://24a0dac0-2579-4138-985c-bec2df4bdfcc-00-3unzo70c406dl.riker.replit.dev/students')
-    .then(response =>{
-        return response.json()
-    })
+fetch(urlStudents)
+    .then(res => res.json())
     .then(data => {
-        console.log(data);
+        console.log(compararEstudiantes(data));
     })
 
+function compararEstudiantes(data){
+    for(let i = 0; i <= data.length; i++){
+        if(data[i].codigo == codigo && data[i].clave == contrasena){
+            return "Inicio sesión";
+        }
+    }
 }
