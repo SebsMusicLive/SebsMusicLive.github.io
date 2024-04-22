727784c5847568f7144aa44f49f3d7d862bbfdb3
SebsMusicLive <lopezortegajohansebastian@gmail.com>
2024-04-22 10:38:12 -0500

    Se realiza el primer commmit

---
+++ js/login.js
@@ -0,0 +1,8 @@
+function ingresar(){
+    var codigo = document.querySelector("#codigo").value;
+    var contrasena = document.querySelector("#contrasena").value;
+}
+
+function estudiantes(){
+    fetch
+}
